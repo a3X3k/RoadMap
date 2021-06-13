b120108facbf25d7ba7ffc7eda3a8f4e2f976cc6
a3X3k <abhishekabi2002@gmail.com>
2021-06-13 21:40:00 +0530

    Update 1.py

--- Set 2/Audio Stego/1.py
+++ Set 2/Audio Stego/1.py
@@ -1,8 +1,22 @@
 import wave
-audio = wave.open("1.wav", mode='rb')
-frame_bytes = bytearray(list(audio.readframes(audio.getnframes())))
+
+file="1.wav"
+
+"""This takes the filepath and decodes the hidden data and returns it"""
+song = wave.open(file, mode='rb')
+
+# Convert audio to byte array
+frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+
+# Extract the LSB of each byte
 extracted = [frame_bytes[i] & 1 for i in range(len(frame_bytes))]
-string = "".join(chr(int("".join(map(str,extracted[i:i+8])),2)) for i in range(0,len(extracted),8))
-decoded = string.split("###")[0]
-print("Sucessfully decoded: "+decoded)
-audio.close()	
+
+# Convert byte array back to string
+message = "".join(chr(int("".join(map(str, extracted[i:i+8])), 2)) for i in range(0, len(extracted), 8))
+
+# Cut off at the filler characters
+decoded = message.split("###")[0]
+
+song.close()
+
+print(decoded)
